88142d0740eb5be3c3c3c5341c31517aa815162f
rarecode00 <ks9192174414@gmail.com>
2025-01-26 22:25:30 +0530

    UI

---
+++ app/layout.js
@@ -0,0 +1,28 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/header";
+import CreateEventDrawer from "@/components/create-event";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Schedulrr",
+  description: " ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header />
+          <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+            {children}
+          </main>         
+          <CreateEventDrawer />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
